b92b3b9b5e6e5dcfe6a6c227dc717c8cdba6e3a1
hamza <hamza.abidi1@esprit.tn>
2022-07-19 14:25:50 +0100

    upload

--- src/main/java/com/smartech/vendorportal/controllers/FileController.java
+++ src/main/java/com/smartech/vendorportal/controllers/FileController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -36,7 +36,7 @@ public class FileController {
 	RfqService rfqService;
 
 	@PostMapping("/upload/{idrfq}")
-	public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") List<MultipartFile> file,
+	public ResponseEntity<ResponseMessage> uploadFile(@RequestPart("file") List<MultipartFile> file,
 			@PathVariable("idrfq") Long idrfq) {
 		String message = "";
 		ResponseEntity<ResponseMessage> re = ResponseEntity.status(HttpStatus.EXPECTATION_FAILED)
