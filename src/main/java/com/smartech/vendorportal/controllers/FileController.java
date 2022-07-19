424a934606fe813eb776bb703ca5246d0e72a9c2
hamza <hamza.abidi1@esprit.tn>
2022-07-19 16:27:46 +0100

    upload

--- src/main/java/com/smartech/vendorportal/controllers/FileController.java
+++ src/main/java/com/smartech/vendorportal/controllers/FileController.java
@@ -10,12 +10,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.smartech.vendorportal.entities.FileDB;
@@ -35,9 +37,10 @@ public class FileController {
 	@Autowired
 	RfqService rfqService;
 
+	@ExceptionHandler(value = MultipartException.class)
 	@PostMapping("/upload/{idrfq}")
-	public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") List<MultipartFile> file,
-			@PathVariable("idrfq") Long idrfq) throws IOException {
+	public ResponseEntity<ResponseMessage> uploadFile(@RequestParam List<MultipartFile> file,
+			@PathVariable("idrfq") Long idrfq) {
 		String message = "";
 		ResponseEntity<ResponseMessage> re = ResponseEntity.status(HttpStatus.EXPECTATION_FAILED)
 				.body(new ResponseMessage("no file to upload"));
