2552e6d989301d21d5603e7de010ae9ddbab0817
hamza <hamza.abidi1@esprit.tn>
2022-08-02 17:19:40 +0100

    config

--- src/main/java/com/smartech/vendorportal/utils/MailConfig.java
+++ src/main/java/com/smartech/vendorportal/utils/MailConfig.java
@@ -2,26 +2,30 @@ package com.smartech.vendorportal.utils;
 
 import java.util.Properties;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+import com.smartech.vendorportal.entities.Config;
+import com.smartech.vendorportal.services.ConfigService;
+
 @Configuration
 public class MailConfig {
-	@Value("${VendorPortal.app.email}")
-	private String email;
-	@Value("${VendorPortal.app.password}")
-	private String password;
+	@Autowired
+	ConfigService configService;
+
 
 	@Bean
 	public JavaMailSender getJavaMailSender() {
+		Config configs = configService.retriveAllConfig();
 		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 		mailSender.setHost("smtp.gmail.com");
 		mailSender.setPort(587);
-		mailSender.setUsername(email);
-		mailSender.setPassword(password);
+		mailSender.setUsername(configs.getEmail());
+		mailSender.setPassword(configs.getPassword());
 		Properties props = mailSender.getJavaMailProperties();
 		props.put("mail.transport.protocol", "smtp");
 		props.put("mail.smtp.auth", "true");
